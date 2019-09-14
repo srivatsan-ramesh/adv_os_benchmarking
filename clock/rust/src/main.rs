8e2762fcc267efa9b2dc1343fe1b4eb91dc0427f
Deepak Sirone Jegan <dsirone@royal-19.cs.wisc.edu>
2019-09-13 19:39:48 -0500

    Changes to rdtsc clock measurement

--- clock/rust/src/main.rs
+++ clock/rust/src/main.rs
@@ -2,7 +2,6 @@
 
 fn main() {
     let mut diffs: [u64; 1000000] = [0; 1000000];
-    let mut k = 0;
     let mut count: u64 = 10;
     let mut diff_early: u64 = 0;
     let mut diff_late: u64 = 0;
@@ -10,27 +9,26 @@ fn main() {
     unsafe {
         for j in 0..diffs.len() {
                 asm!("
-                cpuid\n
                 rdtscp\n
-                #shl rdx, 32\n
-                #add rdx, rax\n": "={rax}"(diff_early)::"rax", "rdx", "rcx", "rbx", "memory": "volatile", "intel");
+                shl rdx, 32\n
+                or rax, rdx\n": "={rax}"(diff_early)::"rax", "rdx", "rcx", "rbx", "memory": "volatile", "intel");
 
                 /*asm!("
                 xor rax, rax\n
                 xor rbx, rbx":::"rax", "rbx":"intel");*/
-                for k1 in 0..10000 {
+                for _k1 in 0..1 {
                     asm!("mov rax, $1\n
                           add rax, 1\n": "={rax}"(count) : "r"(count): "rax", "memory" : "volatile", "intel");
                 }
 
                 asm!("
                 rdtscp\n
-                #shl rdx, 32\n
-                #add rdx, rax\n
+                shl rdx, 32\n
+                or rax, rdx\n
                 ": "={rax}"(diff_late)::"rax", "rdx", "rcx", "rbx", "memory": "volatile", "intel");
 
                 diffs[j] =  diff_late - diff_early;
-                asm!("cpuid":::"rax", "rdx", "rcx", "rbx", "memory": "volatile", "intel");
+                //asm!("cpuid":::"rax", "rdx", "rcx", "rbx", "memory": "volatile", "intel");
         }
 
 
