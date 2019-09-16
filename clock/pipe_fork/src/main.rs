c0e74d69b86e17720a27182f83b994c738070081
Deepak Sirone <deepaksirone94@gmail.com>
2019-09-16 18:06:29 -0500

    Modify pipe_fork

--- clock/pipe_fork/src/main.rs
+++ clock/pipe_fork/src/main.rs
@@ -4,6 +4,17 @@ extern crate libc;
 use libc::{fork, socketpair, write, read, PF_LOCAL, SOCK_STREAM, c_int, c_void};
 const SIZE: usize = 4;
 
+fn prefetch(data: &mut [u8]) {
+    for i in 0..data.len() {
+        data[i] += 1;
+    }
+
+    for j in 0..data.len() {
+        data[j] += 1;
+    }
+}
+
+
 fn main() {
 
     let mut pipe_fds = [0; 2];
@@ -14,21 +25,26 @@ fn main() {
     let parent_fd = 0;
     let child_fd = 1;
 
+    prefetch(&mut data);
     unsafe {
         socketpair(PF_LOCAL, SOCK_STREAM, 0, pipe_fds.as_mut_ptr() as *mut c_int);
         if fork() == 0 {
             let mut total = SIZE;
-            let mut c = read(pipe_fds[child_fd], data.as_mut_ptr() as *mut c_void, total);
+            let mut i = 0;
+            let mut c = read(pipe_fds[child_fd], (data.as_mut_ptr() as *mut c_void).offset(i), total);
             while c > 0 {
                 total = total - c as usize;
-                c = read(pipe_fds[child_fd], data.as_mut_ptr() as *mut c_void, total);
+                i += c;
+                c = read(pipe_fds[child_fd], (data.as_mut_ptr() as *mut c_void).offset(i), total);
             }
 
-            let mut total = SIZE;
-            c = write(pipe_fds[child_fd], data.as_ptr() as *const c_void, total);
+            total = SIZE;
+            i = 0;
+            c = write(pipe_fds[child_fd], (data.as_ptr() as *const c_void).offset(i), total);
             while c > 0 {
                 total = total - c as usize;
-                c = write(pipe_fds[child_fd], data.as_ptr() as *const c_void, total);
+                i += c;
+                c = write(pipe_fds[child_fd], (data.as_ptr() as *const c_void).offset(i), total);
             }
             
         } else {
@@ -37,14 +53,16 @@ fn main() {
                 shl rdx, 32\n
                 or rax, rdx\n": "={rax}"(diff_early)::"rax", "rdx", "rcx", "rbx", "memory": "volatile", "intel");
             let mut total = SIZE;
-            let mut c = write(pipe_fds[parent_fd], data.as_ptr() as *const c_void, total);
+            let mut i = 0;
+            let mut c = write(pipe_fds[parent_fd], (data.as_ptr() as *const c_void).offset(i), total);
             while c > 0 {
                 total = total - c as usize;
                 c = write(pipe_fds[parent_fd], data.as_ptr() as *const c_void, total);
             }
 
-            let mut total = SIZE;
-            let mut c = read(pipe_fds[parent_fd], data.as_mut_ptr() as *mut c_void, total);
+            total = SIZE;
+            i = 0;
+            let mut c = read(pipe_fds[parent_fd], (data.as_mut_ptr() as *mut c_void).offset(i), total);
             while c > 0 {
                 total = total - c as usize;
                 c = read(pipe_fds[parent_fd], data.as_mut_ptr() as *mut c_void, total);
